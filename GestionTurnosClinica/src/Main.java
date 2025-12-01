efcc81c8d267da2f4555dfa0d25d0551b0a175e4
Maximiliano <maximiceli@hotmail.com.ar>
2025-12-01 02:10:10 -0300

    Se agrego la clase Menu, que incorpora las funciones para mostrar el menu y asi tambien los metodos que va a utilizar el menu. Tambien se modifico el Main, haciendo que instancie la clase Clinica y Menu, para ejecutar las funciones del Menu.

--- GestionTurnosClinica/src/Main.java
+++ GestionTurnosClinica/src/Main.java
@@ -1,5 +1,10 @@
+import services.Clinica;
+import utils.MenuClinica;
+
 public class Main {
     public static void main(String[] args) {
-
+        Clinica clinica = new Clinica();
+        MenuClinica menu = new MenuClinica(clinica);
+        menu.iniciar();
     }
 }
